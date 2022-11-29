f82f5a38f62356479725993271f9d579b94028bd
Titas Chowdhury <titaschowdhury3@gmail.com>
2022-11-29 14:06:32 +0530

    commit link

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Home from './pages/Home/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+ import Home from './pages/Home/Home';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap';
+import Register from './componenets/Home/Register/Register';
+// import Register from './componenets/Home/Register/Register';
 // import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // <React.StrictMode>
-    <Home />
+  <BrowserRouter >
+  <Routes>
+  <Route path='/Home' element={<Home />}></Route>
+  <Route path='/register' element={<Register/>}></Route>
+  </Routes>
+  </BrowserRouter>
+    // <Home />
+    // <Register/>
   // </React.StrictMode>
 );
 
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,61 @@
- 
- function Home() {
-    return(
-        <div>This is home Page</div>
-    )
- }
+import { useState } from "react";
+import { useEffect } from "react";
+import ProductCard from "../../componenets/Home/ProductCard/ProductCard";
+import Footer from "../../componenets/Shared/Footer/Footer";
+import Header from "../../componenets/Shared/Header/Header";
 
- export default Home;
+
+function Home() {
+    // Creating a state for products data.
+    // whenever there is a change in products, it will force componenet refresh.
+    const [products, setProducts] = useState([]);
+    // const [click, setClick] = useState(0);
+    // let data = "My Data";
+
+useEffect(()=>{
+    fetch("https://fakestoreapi.com/products")
+    //calling json function.
+    .then((res) => res.json())
+    // listening for json function to return.
+    .then((res) => {
+          console.log("fetching data");
+          res.forEach(o => {
+            o.rating.rate=Math.ceil(Number(o.rating.rate));
+          });
+
+          console.log(res);
+          console.log(products);
+        setProducts(res);
+        //  console.log(products.length)
+        //   console.log("Fetch called")
+    });
+    
+} , [] );
+
+   
+    return (
+        <div>
+
+        <Header/>
+        {/*Products.*/} 
+        <div>
+        {/* <h1>{click}</h1>
+        <button onClick={()=>{setClick(click+1)}}>Click</button>
+        <h3>{ products !== undefined && products.length}</h3> 
+    <h1>{data}</h1> */}
+    {/*Products.*/} 
+    <div className="row">
+    {products.map((product, i)=>(
+        <div className="col-3">
+        < ProductCard item={product} value={i} / > </div>))}
+    </div>
+    
+        
+         </div>  
+        <Footer/>
+        </div>
+    )
+}
+
+
+export default Home;
